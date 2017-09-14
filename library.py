3834dbc29accd40abfaeaa49cb32400803bedf0c
d28035203 <289484834+d28035203@users.noreply.github.com>
2017-09-14 22:08:33 +0530

    feat: issue and return with due dates

--- library.py
+++ library.py
@@ -6,6 +6,7 @@ import sqlite3, sys, os
 from datetime import datetime, timedelta
 
 DB = os.path.join(os.path.dirname(os.path.abspath(__file__)), "library.db")
+LOAN_DAYS = 14
 
 
 def conn():
@@ -51,16 +52,61 @@ def list_books():
  rows = c.execute("SELECT id, title, author, available FROM books").fetchall()
  c.close()
  if not rows:
- print("(no books — empty shelves, empty soul)")
+ print("(no books)")
  return
  for r in rows:
  flag = "IN" if r[3] else "OUT"
  print("[%s] #%d %s — %s" % (flag, r[0], r[1], r[2]))
 
 
+def issue(book_id, borrower):
+ c = conn()
+ row = c.execute("SELECT available FROM books WHERE id=?", (book_id,)).fetchone()
+ if not row:
+ print("book not found")
+ c.close()
+ return
+ if not row[0]:
+ print("already issued — chaos reigns")
+ c.close()
+ return
+ now = datetime.now()
+ due = now + timedelta(days=LOAN_DAYS)
+ c.execute(
+ "INSERT INTO loans(book_id, borrower, issued_on, due_on) VALUES (?,?,?,?)",
+ (book_id, borrower, now.isoformat(timespec="seconds"), due.isoformat(timespec="seconds")),
+ )
+ c.execute("UPDATE books SET available=0 WHERE id=?", (book_id,))
+ c.commit()
+ c.close()
+ print("issued to %s; due %s" % (borrower, due.date()))
+
+
+def return_book(book_id):
+ c = conn()
+ loan = c.execute(
+ "SELECT id, due_on FROM loans WHERE book_id=? AND returned=0",
+ (book_id,),
+ ).fetchone()
+ if not loan:
+ print("no active loan")
+ c.close()
+ return
+ due = datetime.fromisoformat(loan[1])
+ late = datetime.now() > due
+ c.execute("UPDATE loans SET returned=1 WHERE id=?", (loan[0],))
+ c.execute("UPDATE books SET available=1 WHERE id=?", (book_id,))
+ c.commit()
+ c.close()
+ if late:
+ print("returned LATE. the librarian has noted this. forever.")
+ else:
+ print("returned on time. rare. beautiful.")
+
+
 def main(argv):
  if len(argv) < 1:
- print("usage: init | add TITLE AUTHOR | list")
+ print("usage: init | add TITLE AUTHOR | list | issue ID BORROWER | return ID")
  return 1
  cmd = argv[0]
  if cmd == "init":
@@ -69,6 +115,10 @@ def main(argv):
  add_book(argv[1], " ".join(argv[2:]))
  elif cmd == "list":
  list_books()
+ elif cmd == "issue" and len(argv) >= 3:
+ issue(int(argv[1]), argv[2])
+ elif cmd == "return" and len(argv) >= 2:
+ return_book(int(argv[1]))
  else:
  print("unknown command")
  return 1
